04512dd3b28729fbeddb0732bafee059211c0cc7
Chloroformik <121249266+Vertran@users.noreply.github.com>
2026-03-26 13:41:29 +0100

    NOSC

---
+++ client/os/kernel/nucle.py
@@ -0,0 +1,12 @@
+from submodules.interpreter import Interpreter
+from submodules.lexer import Lexer
+from submodules.parser import Parser
+
+tokens = Lexer().start('/home/chlorik/projects/nuclearity/client/os/kernel/def/love_nosc.nosc')
+
+parser = Parser(tokens)
+interpreter = Interpreter()
+
+parsed = parser.parse()
+
+interpreter.run(parsed)
